cbbbd236032f55d8a3b677038e08d22b1d6eacc5
squarekb <bradleysquarek@gmail.com>
2024-01-15 14:47:42 -0500

    Messing around with unmarked package, not much success so far

--- qmee_script.R
+++ qmee_script.R
@@ -1,4 +1,14 @@
+library(unmarked)
+
 data <- read.csv("qmee_data.csv")
-print(data)
 data$tempdiff <- data$water_temp - data$air_temp
-print(data)
+
+#Presence/absence matrix
+y = data[,1:15]
+
+#Site and observation covariates
+siteCovs <- y[,c("site")]
+obsCovs <- list(date=y[,c("edna_occ","wetland_size","pct_pos_qpcr","water_temp","air_temp","pH","salinity","rdo_conc","turbidity","conductivity","canopy_cover","tds","julian_day_eDNA","tempdiff")])
+
+#Creating a special data.frame for analysis in unmarked
+umf <- unmarkedFrameOccu(y = y, siteCovs = siteCovs, obsCovs = obsCovs)
