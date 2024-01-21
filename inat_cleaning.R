e0f1e07b3d238e54323988c8f25cf52e71ac4f13
squarekb <bradleysquarek@gmail.com>
2024-01-20 22:44:23 -0500

    more cleaning to inat dataset, figured out how to replace NA in one column using mutate

--- inat_cleaning.R
+++ inat_cleaning.R
@@ -2,14 +2,18 @@ library(tidyverse)
 library(dplyr)
 
 #import inaturalist data from csv
-inat <- read_csv("iNaturalist_data.csv") 
+raw_inat <- read_csv("iNaturalist_data.csv") 
 
-#cleaning up inaturalist data to select rows of interest and to filter for non-obscured and research grade observations
-clean <- inat %>% 
+#cleaning up inaturalist data to select rows of interest and to filter for non-obscured and research grade observations, replacing NA is common_name by scientific_name
+clean_inat <- raw_inat %>% 
   select(id, observed_on, quality_grade, coordinates_obscured, latitude, longitude, scientific_name, common_name) %>% 
-  filter(quality_grade == "research", coordinates_obscured == "FALSE")
-  
-#trying to figure out how to replace the NA values in the common_name column with the scientific_name value
-  
-## mutate(common_name, if_else(is.na(common_name), common_name = scientific_name, common_name = common_name))
+  filter(quality_grade == "research", coordinates_obscured == "FALSE")%>%
+  mutate(common_name=ifelse(is.na(common_name), scientific_name, common_name))
+
+## most observed species in descending order, Monarch Butterfly is the most observed species
+order <- clean_inat %>%
+  group_by(common_name)%>%
+  summarize(count = n())%>%
+  arrange(desc(count))
+    
 
