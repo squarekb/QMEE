eabdc2a1ba9d4d9ac0662e55e1a8b7cc9b3f7975
Ben Bolker <bbolker@gmail.com>
2024-01-23 20:26:26 -0500

    bmb notes on hw 2

--- inat_cleaning.R
+++ inat_cleaning.R
@@ -7,8 +7,13 @@ raw_inat <- read_csv("iNaturalist_data.csv")
 #cleaning up inaturalist data to select rows of interest and to filter for non-obscured and research grade observations, replacing NA is common_name by scientific_name
 clean_inat <- raw_inat %>% 
   select(id, observed_on, quality_grade, coordinates_obscured, latitude, longitude, scientific_name, common_name) %>% 
-  filter(quality_grade == "research", coordinates_obscured == "FALSE")%>%
+    filter(quality_grade == "research", coordinates_obscured == "FALSE")%>%
+    ## BMB: you might want to use !coordinates_obscured as your filter
+    ##  here since coordinates_obscured is a *logical* vector (! means "not")
   mutate(common_name=ifelse(is.na(common_name), scientific_name, common_name))
+
+## BMB: probably want to drop the coordinates_obscured column once you've
+##  done your filtering
 
 ## most observed species in descending order, Monarch Butterfly is the most observed species
 order <- clean_inat %>%
--- simple_map.R
+++ simple_map.R
@@ -2,4 +2,19 @@
 inat <- readRDS("inat.rds")
 
 ##plotting the locations of inat observation points
+
+## Script won't work without this, hence not reproducible (!)
+## (We're going to be jerks about this)
+library(ggplot2)
 ggplot(data = inat) + geom_point(aes(latitude, longitude), size = 0.5)
+
+## mark: 1.95 (for non-reproducibility)
+
+## You need to be careful about plotting lat/long. For what it's worth,
+##  R has most of the power of a GIS, via the sf package ...
+## the tmap package is also handy
+
+library(sf)
+inat_sf <- st_as_sf(inat, coords = c("longitude", "latitude"), 
+         crs = "WGS84")
+ggplot(inat_sf) + geom_sf(size = 0.5)
