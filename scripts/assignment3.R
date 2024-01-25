31a16aceb7a7031f902ca41f9890a9b3858e699c
Bradley Squarek <127755201+squarekb@users.noreply.github.com>
2024-01-25 17:42:41 -0500

    Update assignment3.R

--- scripts/assignment3.R
+++ scripts/assignment3.R
@@ -3,15 +3,15 @@ library(dplyr)
 library(ggplot2)
 
 #importing data and adding occupancy as a factor, creating a longer dataset with gather()
-data <- (read_csv("qmee_data.csv", col_types=cols())%>%
-           mutate(occupancy = as.factor(edna_occ))%>%
-           gather('wetland_size', 'water_temp','air_temp','pH','salinity','rdo_conc', 'turbidity', 'conductivity', 'canopy_cover', 'tds', 'julian_day_eDNA', key = "env", value = "score")%>%
-           subset(select = -c(edna_occ,pct_pos_qpcr))
+data <- (read_csv("qmee_data.csv", col_types=cols())
+         %>% mutate(occupancy = as.factor(edna_occ))
+         %>% gather('wetland_size', 'water_temp','air_temp','pH','salinity','rdo_conc', 'turbidity', 'conductivity', 'canopy_cover', 'tds', 'julian_day_eDNA', key = "env", value = "score")
+         %>% subset(select = -c(edna_occ,pct_pos_qpcr))
 )
 
 #boxplots of environmental variables by occupancy, scales are adjusted by the bounds of each environmental variable using "free"
-figure <- ggplot(data, aes(x=occupancy, y=score)) +
-  geom_boxplot(aes(fill=occupancy)) +
-  facet_wrap(~env, scales = "free")
+figure <- ggplot(data, aes(x=occupancy, y=score)) 
++ geom_boxplot(aes(fill=occupancy))
++ facet_wrap(~env, scales = "free")
 
 print(figure)
