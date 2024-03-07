400cf8f40840d62b941235ce4a0968c37c11ded8
squarekb <bradleysquarek@gmail.com>
2024-03-07 13:34:57 -0500

    minor changes to glm.R

--- scripts/glm.R
+++ scripts/glm.R
@@ -24,23 +24,23 @@ check_model(glmchem)
 check_model(glmphys)
 check_model(glmall)
 
-#residuals are outside error bounds for one point in glmchem, otherwise all models are ok
+## My residuals are outside error bounds for one point in glmchem, otherwise all models are ok
 
-#inference test, which model predict occupancy more accurately
+#inference test, which model is a better predictor of occupancy
 anova(glmchem,glmphys,glmall)
 
 #according to this anova, model 1 or the chemical model is the best predictor of occupancy
 
-#INTERPRETATION of coefficients: for a one unit change in our coefficients, the odds for the presence of Blanding's turtles is ... As tds increases by one unit, probaility of detection increases by (6.668-1.00)*100 = 566%. Salinity = 399%. Size = 85%. pH, dissolved oxygen, conductivity, day and temperature difference all increase odds of detection slightly. Increases in canopy_pct decrease the odds of detecting Blanding's Turtle slightly.
+#Interpreting logit coefficients: for a one unit change in our coefficients, the odds for the presence of Blanding's turtles is changes by x. As tds increases by one unit, probaility of detection increases by (6.668-1.00)*100 = 566%. Salinity = 399%. Size = 85%. pH, dissolved oxygen, conductivity, day and temperature difference all increase odds of detection slightly. Increases in canopy_pct decrease the odds of detecting Blanding's Turtle slightly.
 exp(glmall$coefficient[2:11])
 
 
-#how much of the variance is explained by the "all" model (I know this was not the best according to ANOVA)?
+#how much of the variance is explained by the "glmall" model? (I know this was not the best according to ANOVA, jut messing around)
 
 # calculate the pseudo-R2
 pseudoR2 <- (glmall$null.deviance - glmall$deviance) / glmall$null.deviance
 pseudoR2
-#6% of the variance in occupancy is explained by this model, that's awful. Good thing these ar epoorly simulated data!
+#6% of the variance in occupancy is explained by this model, that's awful. Good thing these are poorly simulated data!
 
 
 
