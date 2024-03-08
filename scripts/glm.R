8521a88538bd004a1efb4c9a08c7ea68b0910f4b
Jonathan Dushoff <dushoff@mcmaster.ca>
2024-03-08 17:18:57 -0500

    Leaving office

--- scripts/glm.R
+++ scripts/glm.R
@@ -5,7 +5,6 @@ library(dotwhisker)
 library(broom)
 library(effects)
 
-
 #Hypothesis: do chemical or physical variables produce a better model for occupancy?
 
 edna <- (read_csv("data/edna.csv",  col_types=cols())
@@ -19,6 +18,9 @@ glmchem <-glm(occupancy~pH+salinity+do+turbidity+cond+tds, data = edna, family =
 glmphys <-glm(occupancy~day+size+diff+canopy_pct, data = edna, family = binomial)
 glmall <-glm(occupancy ~ pH+salinity+do+turbidity+cond+tds+day+size+diff+canopy_pct, data = edna, family = binomial)
 
+## JD: This seems like the most interesting take-home:
+summary(glmall)
+
 #diagnostics
 check_model(glmchem)
 check_model(glmphys)
@@ -31,6 +33,8 @@ anova(glmchem,glmphys,glmall)
 
 #according to this anova, model 1 or the chemical model is the best predictor of occupancy
 
+## JD: Not really. You could test which one is the best _predictor_ with cross-validation or with information criteria. anova() is aimed at inference: in my interpretation, are we convinced we've seen a clear effect? It's more about parameters than about prediction.
+
 #Interpreting logit coefficients: for a one unit change in our coefficients, the odds for the presence of Blanding's turtles is changes by x. As tds increases by one unit, probaility of detection increases by (6.668-1.00)*100 = 566%. Salinity = 399%. Size = 85%. pH, dissolved oxygen, conductivity, day and temperature difference all increase odds of detection slightly. Increases in canopy_pct decrease the odds of detecting Blanding's Turtle slightly.
 exp(glmall$coefficient[2:11])
 
@@ -42,6 +46,4 @@ pseudoR2 <- (glmall$null.deviance - glmall$deviance) / glmall$null.deviance
 pseudoR2
 #6% of the variance in occupancy is explained by this model, that's awful. Good thing these are poorly simulated data!
 
-
-
-
+## Grade 2/3
